651a57dae69be29887eea0244cafe985a5038e7f
Juan De Dios Perez Lopez <146788593+SpeedLands@users.noreply.github.com>
2024-09-20 20:09:14 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, jsonify
 from app.services.firebase_service import register_user, login_user
 from app.services.plot_service import create_plot_line, create_plot_from_file
 from app.services.pusher_service import send_pusher_event
+from app.services.database_service import buscar_contacto, eliminar_contacto, registrar_contacto, actualizar_contacto, obtener_contacto
 import os
 import pandas as pd
 
@@ -30,16 +31,6 @@ def contact():
 @main.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-@main.route("/alumnos")
-def alumnos():
-    return render_template("alumnos.html")
-
-@main.route("/alumnos/guardar", methods=["POST"])
-def alumnosGuardar():
-    matricula = request.form["txtMatriculaFA"]
-    nombre = request.form["txtNombreApellidoFA"]
-    return f"Matrícula: {matricula} Nombre y Apellido: {nombre}"
 
 @main.route('/register', methods=['POST'])
 def register():
@@ -73,6 +64,42 @@ def trigger_pusher():
 
     return jsonify({'status': 'Event triggered!'}), 200
 
+@main.route('/fetch_contacto', methods=['GET'])
+def ver():
+    return buscar_contacto()
+
+@main.route('/obtener_contacto/<int:id_contacto>', methods=['GET'])
+def api_obtener_contacto(id_contacto):
+    contacto = obtener_contacto(id_contacto)
+    if contacto:
+        return jsonify(contacto), 200
+    else:
+        return jsonify({"mensaje": "Contacto no encontrado."}), 404
+
+@main.route('/add_contacto', methods=['GET'])
+def add_contacto():
+    args = request.args
+    registrar_contacto(args=args)
+    send_pusher_event('my-channel', 'my-event', {'message': 'cambio'})
+    return jsonify({"mensaje": "Contacto registrado exitosamente", "data": args}), 201
+
+@main.route('/actualizar_contacto/<int:id_contacto>', methods=['PUT'])
+def api_actualizar_contacto(id_contacto):
+    # Obtén los datos de la solicitud
+    args = request.get_json()
+
+    # Llama a la función de actualización
+    resultado = actualizar_contacto(id_contacto, args)
+
+    return jsonify(resultado), 200
+
+@main.route('/eliminar_contacto/<int:id_contacto>', methods=['DELETE'])
+def api_eliminar_contacto(id_contacto):
+
+    # Llama a la función de actualización
+    resultado = eliminar_contacto(id_contacto)
+
+    return jsonify(resultado), 200
 
 @main.route('/line', methods=['POST'])
 def line():
